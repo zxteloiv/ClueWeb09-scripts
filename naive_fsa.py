ff3d69aa980e340b7fb3bab3770d85f4bb09f47f
Xiang Zhang <zxteloiv@gmail.com>
2016-01-27 13:35:11 +0800

    finish fsa matching and searching, unit test passed

--- naive_fsa.py
+++ naive_fsa.py
@@ -31,20 +31,52 @@ class FiniteStateAutomata(object):
                 if isinstance(options, dict) and 'max_match_search' in options
                 else 100)
 
+        self.MAX_MATCH_SEARCH = (options['max_match_search']
+                if isinstance(options, dict) and 'max_match_search' in options
+                else 100)
+
+    def search(self, sequence, start=0):
+        """
+        Brute force to search the FSA in the sequence for the first occurence.
+        """
+        self.current = self.start
+        is_success, start_pos, next_start_pos = self.match(sequence, start)
+
+        while start_pos < len(sequence):
+            if is_success:
+                return (True, start_pos, next_start_pos)
+
+            start_pos += 1
+            is_success, start_pos, next_start_pos = self.match(sequence, start_pos)
+
+        return (False, start, next_start_pos)
+
     def match(self, sequence, start=0):
         """
-        Test whether a sequence is matched by the fsa
+        Test whether a sequence is matched by the FSA
+
+        :param sequence , a collection that can be randomly accessed using an index
+        :param start , the start point to search
+
+        :return a tuple of 3 value: (match_success, end_point)
+            If match_success is True, the end_point will be the index right after the tail
+            of the the matched pattern.
+            Otherwise, the end_point will be -1.
         """
+        self.current = self.start
         if start >= len(sequence):
             return (False, start, -1)
 
         pos = start
+        count, previous = 0, None # used to check infinite loops
 
-        count = 0
-        previous = None
-        while pos < len(sequence) and self.current != self.end:
+        while 0 <= pos and pos < len(sequence) and self.current != self.end:
 
-            pos = self.feed(sequence, pos)
+            # When success, pos will increase by 1(default) or length of the matched symbol.
+            # When failed, pos will be at the start point.
+            (is_success, pos) = self._feed(sequence, pos)
+            if is_success: continue
+            else: break
 
             # check if the machine is getting stuck in a infinite loop
             count = count + 1 if previous == self.current else 0
@@ -52,22 +84,22 @@ class FiniteStateAutomata(object):
             if count > self.MAX_MATCH_SEARCH:
                 raise ValueError("Keeping stuck in current state: %s" % str(self.current))
 
-        return ((True, start, pos) if self.current == self.end
-                else (False, start, -1))
+        return ((self.current == self.end), start, pos)
 
-    def transfer_to(self, new_state):
-        print "transfer from", self.current, "to", new_state
+    def _transfer_to(self, new_state):
+        #print "transfer from", self.current, "to", new_state
         self.current = new_state
 
         # move along if any direct transition path exists
         detail = self.states[self.current]
         while 'direct' in detail:
-            print "  --> directly from", self.current, "to", detail['direct']
+            #print "  --> directly from", self.current, "to", detail['direct']
             self.current = detail['direct']
             detail = self.states[self.current]
 
-    def feed(self, sequence, start_pos, matchmode=True):
+    def _feed(self, sequence, start_pos, matchmode=True):
         """
+        Find if the sequence matched any FSA machine
         """
         if self.current not in self.states:
             raise ValueError('State %s cannot be found' % self.current)
@@ -81,22 +113,29 @@ class FiniteStateAutomata(object):
                     # regular expression object symbol
                     for match in symbol.finditer(sequence, start_pos):
                         # consider only the first match
-                        self.transfer_to(target)
-                        return match.end()
+                        if match.start() == start_pos:
+                            self._transfer_to(target)
+                            return (True, match.end())
+                        break
 
                 elif start_pos == sequence.find(symbol, start_pos):
                     # common string
-                    self.transfer_to(target)
-                    return start_pos + len(symbol)
+                    self._transfer_to(target)
+                    return (True, start_pos + len(symbol))
 
                 else:
                     pass
 
         if 'default' in detail:
-            self.transfer_to(detail['default'])
+            self._transfer_to(detail['default'])
+            return (True, start_pos + 1)
 
+        # In match mode, -1 terminates the matching and suggests that no pattern is found.
+        # while in other modes, just move to the next position
         if matchmode:
-            return -1
+            return (False, start_pos)
+        else:
+            return (True, start_pos + 1)
 
 if __name__ == "__main__":
     states = {
@@ -123,8 +162,15 @@ if __name__ == "__main__":
 
     fsa = FiniteStateAutomata(states, start="start", end="end")
 
-    html = "hello abcefef3"
+    html = "hello aefgef3ooo"
+    print "length of", html, "is", len(html)
     print "match '%s' status:" % html, fsa.match(html, 0)
-            
+ 
+    html = "aefgef3"
+    print "length of", html, "is", len(html)
+    print "match '%s' status:" % html, "\n", fsa.match(html, 0)
 
+    html = "hello aefgef3ooo"
+    print "length of", html, "is", len(html)
+    print "match '%s' status:" % html, fsa.search(html, 0)
 
