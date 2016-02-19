037e708d8a5a0d4b7dee4f8c9c12c5d9d7dd6978
zxteloiv <zxteloiv@gmail.com>
2016-02-19 18:24:43 +0800

    add options to checktail utility

--- checktail.sh
+++ checktail.sh
@@ -1,20 +1,49 @@
 #!/bin/bash
 
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 dirname(en0041) fileid(13) [opt]"
+    echo "	by default opt will be 'eosa'"
+    echo "	e: error, o: out, s: state, a: annsfile"
+    exit;
+fi
+
 dirnum=$1
 fileid=$2
 
-echo "err===>"
-tail -5 dump/${dirnum}_${fileid}.err 
+opt=$3
 
-echo ""
-echo "out===>"
-tail -5 dump/${dirnum}_${fileid}.out 
+if [ ! -n $opt ]; then
+    opt="eosa"
+fi
 
-echo ""
-echo "state===>"
-tail -5 dump/state/${dirnum}_${fileid}
-echo ""
+case "$opt" in
+    *e*)
+        echo "err===>"
+        tail -5 dump/${dirnum}_${fileid}.err 
+        ;;
+esac
 
-echo ""
-echo "anns.tsv===>"
-tail -5 ~/data/FACC1/ClueWeb09_English_4/${dirnum}/${fileid}.anns.tsv 
+case "$opt" in
+    *o*)
+        echo ""
+        echo "out===>"
+        tail -5 dump/${dirnum}_${fileid}.out 
+        ;;
+esac
+
+case "$opt" in
+    *s*)
+        echo ""
+        echo "state===>"
+        tail -5 dump/state/${dirnum}_${fileid}
+        echo ""
+        ;;
+esac
+
+case "$opt" in
+    *a*)
+        echo ""
+        echo "anns.tsv===>"
+        tail -5 ~/data/FACC1/ClueWeb09_English_4/${dirnum}/${fileid}.anns.tsv 
+        ;;
+esac
